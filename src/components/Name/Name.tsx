8cd8c6c452b48664dba11d58b21657c29e89e88b
Graham Holtslander <gholtslander@vendasta.com>
2020-05-29 21:59:25 -0600

    chore: set line width to 80 and set tab size to 2

--- src/components/Name/Name.tsx
+++ src/components/Name/Name.tsx
@@ -2,53 +2,49 @@ import React, { useState, useEffect, SyntheticEvent } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-type NameProps = { name?: string, onChange: (name: string) => void};
+type NameProps = { name?: string; onChange: (name: string) => void };
 
 export default function Name(props: NameProps) {
-    const [editing, setEditing] = useState(false);
-    const [localName, setLocalName] = useState(props.name || '');
-    useEffect(() => {
-        if (props.name !== '') {
-            setLocalName(props.name || '');
-        }
-    }, [props.name]);
-
-    function handleSubmit(e: SyntheticEvent) {
-        e.preventDefault();
-        handleClick();
+  const [editing, setEditing] = useState(false);
+  const [localName, setLocalName] = useState(props.name || '');
+  useEffect(() => {
+    if (props.name !== '') {
+      setLocalName(props.name || '');
     }
+  }, [props.name]);
 
-    function handleClick() {
-        setEditing(false);
-        props.onChange(localName || '');
-    }
+  function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+    handleClick();
+  }
 
-    let nameArea;
-    if (editing) {
-        nameArea = (
-            <Form inline onSubmit={handleSubmit}>
-                <Form.Control
-                    type="text"
-                    value={localName}
-                    onChange={(event) => setLocalName(event.target.value)}
-                ></Form.Control>
-                <Button
-                    onClick={handleClick}
-                >
-                    Save
-                </Button>
-            </Form>
-        );
-    } else {
-        nameArea = (
-            <span onClick={() => setEditing(true)}>
-                {!!props.name ? props.name : 'Your name'}
-            </span>
-        );
-    }
-    return (
-        <>
-            <strong>{nameArea}</strong>
-        </>
+  function handleClick() {
+    setEditing(false);
+    props.onChange(localName || '');
+  }
+
+  let nameArea;
+  if (editing) {
+    nameArea = (
+      <Form inline onSubmit={handleSubmit}>
+        <Form.Control
+          type="text"
+          value={localName}
+          onChange={(event) => setLocalName(event.target.value)}
+        ></Form.Control>
+        <Button onClick={handleClick}>Save</Button>
+      </Form>
     );
+  } else {
+    nameArea = (
+      <span onClick={() => setEditing(true)}>
+        {!!props.name ? props.name : 'Your name'}
+      </span>
+    );
+  }
+  return (
+    <>
+      <strong>{nameArea}</strong>
+    </>
+  );
 }
