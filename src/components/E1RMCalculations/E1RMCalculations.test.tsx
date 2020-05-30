c758d049aeafb1d81e5235ba8a13372c2f9586fd
Graham Holtslander <gholtslander@vendasta.com>
2020-05-30 09:27:13 -0600

    test: tests for e1rmcalculations and formatting

--- src/components/E1RMCalculations/E1RMCalculations.test.tsx
+++ src/components/E1RMCalculations/E1RMCalculations.test.tsx
@@ -1,36 +1,74 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import E1RMCalculations from './E1RMCalculations';
 import { Calculation } from '../../reducers/e1rm.reducer';
 
 const mockCalculations: Calculation[] = [
-    {
-        e1rm: '201',
-        lift: 'squat',
-        weight: 150,
-        reps: 5,
-        rpe: 7,
-    },
-    {
-        e1rm: '200',
-        lift: 'squat',
-        weight: 150,
-        reps: 5,
-        rpe: 7,
-    },
+  {
+    e1rm: '201',
+    lift: 'squat',
+    weight: 150,
+    reps: 5,
+    rpe: 7,
+  },
+  {
+    e1rm: '200',
+    lift: 'squat',
+    weight: 150,
+    reps: 5,
+    rpe: 7,
+  },
+  {
+    e1rm: '140',
+    lift: 'ohp',
+    weight: 110,
+    reps: 5,
+    rpe: 8,
+  },
 ];
 
 describe('E1RMCalculations', () => {
-    it('renders nothing if it gets 0 calculations', () => {
-        const { getByTestId } = render(<E1RMCalculations calculations={[]} />);
-        const empty = getByTestId('no-data');
-        expect(empty.textContent).toEqual('');
-    });
+  it('should render a helper message if it gets 0 calculations', () => {
+    const { getByText } = render(<E1RMCalculations calculations={[]} />);
+    const empty = getByText(/You haven't calculated an estimated 1RM yet/);
+    expect(empty).not.toBeNull();
+  });
 
-    it('renders heading if it gets two calculations', () => {
-        const { getByRole } = render(<E1RMCalculations calculations={mockCalculations} />);
-        const heading = getByRole('heading', { name: /Previously Calculated E1RMs/ });
-        expect(heading.textContent).not.toEqual('');
+  it('should render a heading if it gets one or more calculations', () => {
+    const { getByRole } = render(
+      <E1RMCalculations calculations={mockCalculations} />
+    );
+    const heading = getByRole('heading', {
+      name: /Previously Calculated E1RMs/,
     });
+    expect(heading.textContent).not.toEqual('');
+  });
+
+  it('should filter the e1rms by the lift selected', async () => {
+    const { getByRole, getByText, findAllByRole } = render(
+      <E1RMCalculations calculations={mockCalculations} />
+    );
+
+    // Get all the current table rows
+    let tableRows = await findAllByRole('row');
+    // Table rows includes the header row
+    expect(tableRows.length).toEqual(mockCalculations.length + 1);
+
+    // Set the filter to bench
+    const select = getByRole('combobox');
+    fireEvent.change(select, {
+      target: { value: 'bench' },
+    });
+    // No lifts in the mockCalculations match this filter, a message is shown.
+    expect(getByText(/E1RMs for this lift/i)).toBeInTheDocument();
+
+    // Switch the filter to overhead press, which has an e1rm
+    fireEvent.change(select, {
+      target: { value: 'ohp' },
+    });
+    tableRows = await findAllByRole('row');
+    // Expect that there will be the one OHP row + the header row
+    expect(tableRows.length).toEqual(2);
+  });
 });
--- src/components/E1RMCalculations/E1RMCalculations.tsx
+++ src/components/E1RMCalculations/E1RMCalculations.tsx
@@ -8,57 +8,83 @@ import { Calculation } from '../../reducers/e1rm.reducer';
 import './E1RMCalculations.css';
 
 interface E1RMCalculationsProps {
-    calculations: Calculation[];
+  calculations: Calculation[];
 }
 
-export default function E1RMCalculations({ calculations }: E1RMCalculationsProps) {
-    const [liftFilter, setLiftFilter] = useState('');
-    if (calculations.length < 1) {
-        return <span data-testid="no-data">
-            You haven't calculated an estimated 1RM yet! Try out the form to your left <span role="img" aria-label="eyes">👀</span>
-        </span>;
-    }
-    const now = new Date();
-    if (liftFilter !== '') {
-        calculations = calculations.filter(calc => calc.lift === liftFilter);
-    }
-    const listItems = calculations.map((calc: Calculation) => <tr key={JSON.stringify(calc)}>
-        <td>{calc.created ? formatDistance(new Date(calc.created), now) + ' ago' : 'Unknown'}</td>
-        <td>{calc.e1rm}</td>
-        <td>{calc.lift}</td>
-        <td>{calc.weight}</td>
-        <td>{calc.reps}</td>
-        <td>{calc.rpe}</td>
-    </tr>);
-
+export default function E1RMCalculations({
+  calculations,
+}: E1RMCalculationsProps) {
+  const [liftFilter, setLiftFilter] = useState('');
+  if (calculations.length < 1) {
     return (
-        <div className="calculations">
-            <h2 className="calculations__heading">
-                Previously Calculated E1RMs
-                <Form.Control className="lift-selector" as="select" required={true} value={liftFilter} onChange={(event) => setLiftFilter(event.target.value)}>
-                    <option value="">All lifts</option>
-                    <option value="squat">Squat</option>
-                    <option value="bench">Bench</option>
-                    <option value="deadlift">Deadlift</option>
-                    <option value="ohp">Overhead Press</option>
-                    <option value="other">Other</option>
-                </Form.Control>
-            </h2>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>Recorded</th>
-                        <th>E1RM</th>
-                        <th>Lift</th>
-                        <th>Weight</th>
-                        <th>Reps</th>
-                        <th>RPE</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listItems}
-                </tbody>
-            </Table>
-        </div>
+      <span>
+        You haven't calculated an estimated 1RM yet! Try out the form to your
+        left{' '}
+        <span role="img" aria-label="eyes">
+          👀
+        </span>
+      </span>
     );
+  }
+  const now = new Date();
+  if (liftFilter !== '') {
+    calculations = calculations.filter((calc) => calc.lift === liftFilter);
+  }
+  const listItems = calculations.map((calc: Calculation) => (
+    <tr key={JSON.stringify(calc)}>
+      <td>
+        {calc.created
+          ? formatDistance(new Date(calc.created), now) + ' ago'
+          : 'Unknown'}
+      </td>
+      <td>{calc.e1rm}</td>
+      <td>{calc.lift}</td>
+      <td>{calc.weight}</td>
+      <td>{calc.reps}</td>
+      <td>{calc.rpe}</td>
+    </tr>
+  ));
+
+  return (
+    <div className="calculations">
+      <h2 className="calculations__heading">
+        Previously Calculated E1RMs
+        <Form.Control
+          className="lift-selector"
+          as="select"
+          required={true}
+          value={liftFilter}
+          onChange={(event) => setLiftFilter(event.target.value)}
+        >
+          <option value="">All lifts</option>
+          <option value="squat">Squat</option>
+          <option value="bench">Bench</option>
+          <option value="deadlift">Deadlift</option>
+          <option value="ohp">Overhead Press</option>
+          <option value="other">Other</option>
+        </Form.Control>
+      </h2>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Recorded</th>
+            <th>E1RM</th>
+            <th>Lift</th>
+            <th>Weight</th>
+            <th>Reps</th>
+            <th>RPE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listItems.length > 0 ? (
+            listItems
+          ) : (
+            <tr>
+              <td colSpan={6}>{"You don't have any E1RMs for this lift!"}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
