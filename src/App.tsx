d28e2b34515df7c4c7b1eb50acb298d819a5f3e0
Graham Holtslander <gholtslander@vendasta.com>
2020-05-31 09:00:02 -0600

    fix: fetch e1rms again once userId is found

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useReducer, useEffect, FormEvent, useState, useRef } from 'react';
+import React, {
+  useReducer,
+  useEffect,
+  FormEvent,
+  useState,
+  useRef,
+} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -14,140 +20,167 @@ import Name from './components/Name/Name';
 
 import { reducer, State } from './reducers/e1rm.reducer';
 
+require('dotenv').config();
+
+const useDark = true;
+const darkRoot = 'https://ghostlander-e1rm.builtwithdark.com';
+
 export const initialState: State = {
-    reps: 1,
-    weight: 0,
-    rpe: 8,
-    calculations: [],
-    lastCalculation: undefined,
-    loaded: false,
-    lift: '',
+  reps: 1,
+  weight: 0,
+  rpe: 8,
+  calculations: [],
+  lastCalculation: undefined,
+  loaded: false,
+  lift: '',
 };
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [user, setUser] = useState({
-        userId: '',
-        name: ''
-    });
-    const userSetName = useRef(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [user, setUser] = useState({
+    userId: '',
+    name: '',
+  });
+  const userSetName = useRef(false);
 
-    useEffect(() => {
-        if (user.userId !== '') {
-            return;
-        }
-        let { userId } = localStorage;
-        if (!!userId) {
-            async function fetchUser(uId: string) {
-                const response = await axios.get('/user?userId=' + uId);
-                console.log(response.data);
-                setUser(response.data);
-            }
-            fetchUser(userId);
-            return;
-        }
-        // If they don't already have one, let's generate them one.
-        userId = uuidv4();
-        async function createUser(uId: string) {
-            const response = await axios.post('/user', { userId: uId });
-            if (response.status === 200) {
-                setUser(u => {
-                    u.userId = uId;
-                    return u;
-                });
-                localStorage.userId = uId;
-            }
-        }
-        createUser(userId);
-    }, [user.userId]);
-
-    useEffect(() => {
-        if (state.loaded) {
-            return;
-        }
-        if (user.userId === '') {
-            dispatch({ type: 'load-calculations', payload: [] });
-            return;
-        }
-        async function fetchData() {
-            console.log(user.userId);
-            const response = await axios.get('/e1rms?userId=' + user.userId);
-            dispatch({ type: 'load-calculations', payload: response.data });
-        }
-        fetchData();
-    }, [state.loaded, user.userId]);
-
-    useEffect(() => {
-        if (user.name === '') {
-            return;
-        }
-        if (!userSetName.current) {
-            return;
-        }
-        axios.post('/user', {
-            userId: user.userId,
-            name: user.name,
-        });
-    }, [user.name, user.userId]);
-
-    useEffect(() => {
-        if (!state.lastCalculation) {
-            return;
-        }
-        axios.post('/e1rm', {
-            ...state.lastCalculation,
-            userId: user.userId,
-        });
-    }, [state.lastCalculation, user.userId]);
-
-    let calculationsArea = <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
-    </Spinner>;
-    if (state.loaded) {
-        calculationsArea = <E1RMCalculations calculations={state.calculations} />;
+  useEffect(() => {
+    if (user.userId !== '') {
+      return;
     }
+    let { userId } = localStorage;
+    if (!!userId) {
+      async function fetchUser(uId: string) {
+        let url = '/user?userId=' + uId;
+        if (useDark) {
+          url = darkRoot + url;
+        }
+        const response = await axios.get(url);
+        console.log(response.data);
+        setUser(response.data);
+      }
+      fetchUser(userId);
+      return;
+    }
+    // If they don't already have one, let's generate them one.
+    userId = uuidv4();
+    async function createUser(uId: string) {
+      let url = '/user';
+      if (useDark) {
+        url = darkRoot + url;
+      }
+      const response = await axios.post(url, { userId: uId });
+      if (response.status === 200) {
+        setUser((u) => {
+          u.userId = uId;
+          return u;
+        });
+        localStorage.userId = uId;
+      }
+    }
+    createUser(userId);
+  }, [user.userId]);
 
-    return (
-        <Container fluid>
-            <Row>
-                <Col>
-                    <Navbar bg="light" className="justify-content-between">
-                        <Navbar.Brand href="#home">Estimated 1 rep max calculator</Navbar.Brand>
-                        <span>
-                            E1RMs for <Name
-                                name={user.name}
-                                onChange={(name: string) => {
-                                    userSetName.current = true;
-                                    setUser({ ...user, name });
-                                }}
-                            />
-                        </span>
-                    </Navbar>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={4}>
-                    <E1RMForm
-                        weight={state.weight}
-                        reps={state.reps}
-                        rpe={state.rpe}
-                        lift={state.lift}
-                        setWeight={(weight: number) => dispatch({ type: 'weight', payload: weight })}
-                        setReps={(reps: number) => dispatch({ type: 'reps', payload: reps })}
-                        setRpe={(rpe: number) => dispatch({ type: 'rpe', payload: rpe })}
-                        setLift={(lift: string) => dispatch({ type: 'set-lift', payload: lift })}
-                        onSubmit={(e: FormEvent) => {
-                            e.preventDefault();
-                            dispatch({ type: 'calculate' });
-                        }}
-                    />
-                </Col>
-                <Col md={8}>
-                    {calculationsArea}
-                </Col>
-            </Row>
-        </Container>
-    );
+  useEffect(() => {
+    if (state.loaded) {
+      return;
+    }
+    if (user.userId === '') {
+      // don't set loaded to true - this way we can still refetch if a userId is found later
+      dispatch({ type: 'load-calculations-no-user' });
+      return;
+    }
+    async function fetchData() {
+      console.log(user.userId);
+      let url = '/e1rms?userId=' + user.userId;
+      if (useDark) {
+        url = darkRoot + url;
+      }
+      const response = await axios.get(url);
+      dispatch({ type: 'load-calculations', payload: response.data });
+    }
+    fetchData();
+  }, [state.loaded, user.userId]);
+
+  useEffect(() => {
+    if (user.name === '') {
+      return;
+    }
+    if (!userSetName.current) {
+      return;
+    }
+    axios.post('/user', {
+      userId: user.userId,
+      name: user.name,
+    });
+  }, [user.name, user.userId]);
+
+  useEffect(() => {
+    if (!state.lastCalculation) {
+      return;
+    }
+    axios.post('/e1rm', {
+      ...state.lastCalculation,
+      userId: user.userId,
+    });
+  }, [state.lastCalculation, user.userId]);
+
+  let calculationsArea = (
+    <Spinner animation="border" role="status">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
+  );
+  if (state.loaded) {
+    calculationsArea = <E1RMCalculations calculations={state.calculations} />;
+  }
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <Navbar bg="light" className="justify-content-between">
+            <Navbar.Brand href="#home">
+              Estimated 1 rep max calculator
+            </Navbar.Brand>
+            <span>
+              E1RMs for{' '}
+              <Name
+                name={user.name}
+                onChange={(name: string) => {
+                  userSetName.current = true;
+                  setUser({ ...user, name });
+                }}
+              />
+            </span>
+          </Navbar>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <E1RMForm
+            weight={state.weight}
+            reps={state.reps}
+            rpe={state.rpe}
+            lift={state.lift}
+            setWeight={(weight: number) =>
+              dispatch({ type: 'weight', payload: weight })
+            }
+            setReps={(reps: number) =>
+              dispatch({ type: 'reps', payload: reps })
+            }
+            setRpe={(rpe: number) => dispatch({ type: 'rpe', payload: rpe })}
+            setLift={(lift: string) =>
+              dispatch({ type: 'set-lift', payload: lift })
+            }
+            onSubmit={(e: FormEvent) => {
+              e.preventDefault();
+              dispatch({ type: 'calculate' });
+            }}
+          />
+        </Col>
+        <Col md={8}>{calculationsArea}</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
--- src/reducers/e1rm.reducer.ts
+++ src/reducers/e1rm.reducer.ts
@@ -1,64 +1,67 @@
 import { calculateE1RM } from '../e1rm/calculate';
 
 export interface Calculation {
-    weight: number;
-    rpe: number;
-    reps: number;
-    lift: string;
-    e1rm: string;
-    created?: Date;
+  weight: number;
+  rpe: number;
+  reps: number;
+  lift: string;
+  e1rm: string;
+  created?: Date;
 }
 
 export interface State {
-    weight: number;
-    rpe: number;
-    reps: number;
-    lift: string;
-    loaded: boolean;
-    calculations: Calculation[];
-    lastCalculation?: Calculation;
+  weight: number;
+  rpe: number;
+  reps: number;
+  lift: string;
+  loaded: boolean;
+  calculations: Calculation[];
+  lastCalculation?: Calculation;
 }
 
 // Discriminated union 💁🏼‍♂️ https://www.typescriptlang.org/docs/handbook/advanced-types.html#discriminated-unions
 export type Actions =
-    | { type: 'weight'; payload: number }
-    | { type: 'reps'; payload: number }
-    | { type: 'rpe'; payload: number }
-    | { type: 'set-lift'; payload: string }
-    | { type: 'load-calculations'; payload: Calculation[] }
-    | { type: 'calculate' };
+  | { type: 'weight'; payload: number }
+  | { type: 'reps'; payload: number }
+  | { type: 'rpe'; payload: number }
+  | { type: 'set-lift'; payload: string }
+  | { type: 'load-calculations'; payload: Calculation[] }
+  | { type: 'load-calculations-no-user' }
+  | { type: 'calculate' };
 
 export function reducer(state: State, action: Actions): State {
-    switch (action.type) {
-        case 'weight':
-            return { ...state, weight: action.payload };
-        case 'reps':
-            return { ...state, reps: action.payload };
-        case 'rpe':
-            return { ...state, rpe: action.payload };
-        case 'set-lift':
-            return { ...state, lift: action.payload };
-        case 'calculate':
-            const e1rm = calculateE1RM(state.weight, state.rpe, state.reps);
-            if (e1rm === 0) {
-                return state;
-            }
-            const calculation: Calculation = {
-                reps: state.reps,
-                weight: state.weight,
-                rpe: state.rpe,
-                lift: state.lift,
-                e1rm: e1rm.toFixed(2),
-                created: new Date(),
-            };
-            return {
-                ...state,
-                lastCalculation: calculation,
-                calculations: [calculation, ...state.calculations],
-            };
-        case 'load-calculations':
-            return { ...state, loaded: true, calculations: action.payload };
-        default:
-            throw new Error();
-    }
+  switch (action.type) {
+    case 'weight':
+      return { ...state, weight: action.payload };
+    case 'reps':
+      return { ...state, reps: action.payload };
+    case 'rpe':
+      return { ...state, rpe: action.payload };
+    case 'set-lift':
+      return { ...state, lift: action.payload };
+    case 'calculate':
+      const e1rm = calculateE1RM(state.weight, state.rpe, state.reps);
+      if (e1rm === 0) {
+        return state;
+      }
+      const calculation: Calculation = {
+        reps: state.reps,
+        weight: state.weight,
+        rpe: state.rpe,
+        lift: state.lift,
+        e1rm: e1rm.toFixed(2),
+        created: new Date(),
+      };
+      return {
+        ...state,
+        lastCalculation: calculation,
+        calculations: [calculation, ...state.calculations],
+      };
+    case 'load-calculations':
+      return { ...state, loaded: true, calculations: action.payload };
+    case 'load-calculations-no-user':
+      return { ...state, calculations: [] };
+    default:
+      throw new Error();
+  }
 }
